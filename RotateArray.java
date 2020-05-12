4e1f3a15f99b32d5a22af86d63e3d58b937a958d
Lorne Zhang <lz150@duke.edu>
2020-05-12 01:01:35 -0700

    Solved Rotate Array in O(n*k) time

---
+++ RotateArray.java
@@ -0,0 +1,15 @@
+public class RotateArray {
+    /**
+     * Runtime complexity: O(n*k)
+     * Space complexity: O(1)
+     */
+    public void rotate(int[] nums, int k) {
+        for(int i=0; i<k; i++){
+            int last = nums[nums.length-1];
+            for(int j=nums.length-1; j>0; j--){
+                nums[j] = nums[j-1];
+            }
+            nums[0] = last;
+        }
+    }
+}
